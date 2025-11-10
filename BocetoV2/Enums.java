b1cc18469f38d463ab337c1bb73943d489d44798
Victor Tietje Fonollosa <vtietjefonollosa@gmail.com>
2025-11-10 09:24:37 +0100

    Creacion carpetaV2

---
+++ BocetoV2/Enums.java
@@ -0,0 +1,21 @@
+public enum EtiquetaTarea {
+    TRABAJO,
+    PERSONAL,
+    ESTUDIO,
+    HOGAR,
+    SALUD,
+    OTROS
+}
+
+public enum TipoTransaccion {
+    GASTO,
+    INGRESO
+}
+
+public enum IconoEstadoAnimo {
+    GENIAL,
+    BIEN,
+    NORMAL,
+    MAL,
+    MUYMAL
+}
