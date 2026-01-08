6461032795a9ccd85bd490960fc38bb17e7b995a
Cosme Juan Rodriguez Perez <cosmerod2006@gmail.com>
2026-01-08 08:57:03 +0100

    Actu diario

--- ProyectoEx/obj/Debug/net9.0/ProyectoEx.AssemblyInfo.cs
+++ ProyectoEx/obj/Debug/net9.0/ProyectoEx.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoEx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08ef6aae3f5e72ac53affe4501a6db207257c5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a59142d4cf9c20f228dc67bc36c044eb54b201f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoEx")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoEx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
