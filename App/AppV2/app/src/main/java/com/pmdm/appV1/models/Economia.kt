8f756c7f9feb51099a385a8c162e69fcbe7f6146
Victor <victiefon@alu.edu.gva.es>
2026-01-15 08:39:43 +0100

    añadiendo economia

--- App/AppV2/app/src/main/java/com/pmdm/appV1/models/Economia.kt
+++ App/AppV2/app/src/main/java/com/pmdm/appV1/models/Economia.kt
@@ -1,6 +1,32 @@
 package com.pmdm.appV1.models
 
-data class Economia(
-    var saldo: Double = 0.0,
-    var historialTransacciones: MutableList<Transaccion> = mutableListOf()
-)
+class Economia(private val economiaDao: EconomiaDao) {
+    var saldo: Double = 0.0
+        private set
+
+    init {
+        saldo = calcularSaldoInicial()
+    }
+
+    private fun calcularSaldoInicial(): Double {
+        val transacciones = economiaDao.obtenerTodasLasTransacciones()
+        val ingresos = transacciones.filter { it.tipo == TipoTransaccion.INGRESO }.sumOf { it.cantidad }
+        val gastos = transacciones.filter { it.tipo == TipoTransaccion.GASTO }.sumOf { it.cantidad }
+        return ingresos - gastos
+    }
+
+    fun agregarTransaccion(transaccion: Transaccion) {
+        // En Android, esto se haría usualmente a través de un ViewModel
+        economiaDao.guardarTransaccion(transaccion)
+
+        if (transaccion.tipo == TipoTransaccion.INGRESO) {
+            saldo += transaccion.cantidad
+        } else {
+            saldo -= transaccion.cantidad
+        }
+    }
+
+    // Métodos de filtrado que retornan listas para la UI
+    fun obtenerGastos(): List<Transaccion> = economiaDao.obtenerTransaccionesPorTipo(TipoTransaccion.GASTO)
+    fun obtenerIngresos(): List<Transaccion> = economiaDao.obtenerTransaccionesPorTipo(TipoTransaccion.INGRESO)
+}
